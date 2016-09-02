e94122481d54782691efa4b37eecc12447dbe80a
Harini Sree.R <harinisree.chandran@gmail.com>
2016-09-02 12:24:07 +0530

    Create p.java

---
+++ p.java
@@ -0,0 +1,23 @@
+import.java.util.Scanner; 
+public class Positive_Negative 
+{ 
+public statis void main(String[] args) 
+{ 
+int a; 
+Scanner s=new Scanner(System.in); 
+System.out.println("Enetr the number:");
+n=s.nextInt();
+if(a>0) 
+{ 
+System.out.println("The number is positive"); 
+} 
+else if(a<0) 
+{ 
+System.out.println("The number is negative"); 
+} 
+else  
+{ 
+System.out.println("The number is zero"); 
+} 
+}
+}
